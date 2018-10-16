bb10bb4e3c17e2788f88c71d94a133ceaecd5009
JaanJahimees <jaanjahilo@outlook.com>
2018-10-16 11:14:20 +0300

    Enable/Disable button after sending miner

--- addvalue.js
+++ addvalue.js
@@ -1,9 +1,11 @@
 var userStone = 0;
 var currentStone = 0;
 var stoneText = 'You have ' + currentStone.toString() + ' stone';
+var minerSent = false;
 
 document.getElementById('YourStone').innerHTML = stoneText;
 function MinerWork(){
+    DisableSendMiner();
     console.log("Miner sent to work")
     setTimeout(function() {
         userStone += 1;
@@ -11,5 +13,20 @@ function MinerWork(){
         stoneText = 'You have ' + currentStone.toString() + ' stone'
         document.getElementById('YourStone').innerHTML = stoneText
         console.log("Miner done working");
+        EnableSendMiner();
     }, 5000, console.log("Miner is working..."));
+}
+
+function DisableSendMiner(){
+    if(!minerSent){
+        minerSent= true;
+        document.getElementById("sendMinerBtn").disabled = true;
+    }
+}
+
+function EnableSendMiner(){
+    if(minerSent){
+        minerSent= false;
+        document.getElementById("sendMinerBtn").disabled = false;
+    }
 }
